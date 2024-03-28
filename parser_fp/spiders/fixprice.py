b2e20804e8d5e0d1d1792f8933c99de3437375a0
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2024-03-29 00:11:05 +0300

    edit start_requests and parse methods

--- parser_fp/spiders/fixprice.py
+++ parser_fp/spiders/fixprice.py
@@ -1,10 +1,38 @@
 import scrapy
+from ..items import ProductItem
 
 
 class FixpriceSpider(scrapy.Spider):
     name = "fixprice"
     allowed_domains = ["fix-price.com"]
-    start_urls = ["https://fix-price.com"]
+    start_urls = [
+        "https://fix-price.com/catalog/kosmetika-i-gigiena/ukhod-za-polostyu-rta",
+        "https://fix-price.com/catalog/kosmetika-i-gigiena/gigienicheskie-sredstva",
+        "https://fix-price.com/catalog/krasota-i-zdorove/dlya-tela"
+    ]
+
+    def start_requests(self):
+        """
+        Метод вызова стартовых адресов из списка.
+        Передает объект Request по каждому адресу в метод parse().
+        """
+        for url in self.start_urls:
+            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        pass
+        """
+        Метод обратного вызова.
+        Извлекает информацию о каждом товаре по каждой категории.
+        """
+        products = response.css('div.product__wrapper')
+        for product in products:
+            product_data = {
+                'title': response.css('a.title::text').get(),
+            }
+        yield ProductItem(product_data)
+
+
+
+        #     item = ProductItem()
+        #     item['title'] = response.css('div.product__wrapper a.title::text').get()
+        # yield item
