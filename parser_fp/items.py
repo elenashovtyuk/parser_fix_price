a7d8455e9276d0a7388ca79faa82acb87c65aa24
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2024-03-28 20:41:32 +0300

    create ProductItem for return info about products

--- parser_fp/items.py
+++ parser_fp/items.py
@@ -1,12 +1,16 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
-class ParserFpItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class ProductItem(scrapy.Item):
+    timestamp = scrapy.Field()
+    rpc = scrapy.Field()
+    url = scrapy.Field()
+    title = scrapy.Field()
+    marketing_tags = scrapy.Field()
+    brand = scrapy.Field()
+    section = scrapy.Field()
+    price_data = scrapy.Field()
+    stock = scrapy.Field()
+    assets = scrapy.Field()
+    metadata = scrapy.Field()
+    variants = scrapy.Field()
