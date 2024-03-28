5366e4035796114ecc1a58381ce3275d1ab1c6b8
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2024-03-29 02:08:54 +0300

    edit parse methods

--- parser_fp/spiders/fixprice.py
+++ parser_fp/spiders/fixprice.py
@@ -1,4 +1,5 @@
 import scrapy
+import urllib.parse
 from ..items import ProductItem
 
 
@@ -17,22 +18,49 @@ class FixpriceSpider(scrapy.Spider):
         Передает объект Request по каждому адресу в метод parse().
         """
         for url in self.start_urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+            yield scrapy.Request(url=url, callback=self.parse_link)
 
-    def parse(self, response):
+    def parse_link(self, response):
         """
         Метод обратного вызова.
-        Извлекает информацию о каждом товаре по каждой категории.
+        Извлекает ссылки на каждый товар из карточек товара.
         """
-        products = response.css('div.product__wrapper')
-        for product in products:
-            product_data = {
-                'title': response.css('a.title::text').get(),
-            }
-        yield ProductItem(product_data)
+        # получаем div с карточками продуктов
+        cards = response.css('div.product__wrapper')
+
+        product_urls = []
+        # из каждой карточки в div достаем ссылку на адрес страницы продукта
+        for card in cards:
+            product_url = response.css(
+                'div.product__wrapper a.title::attr(href)').get()
+            # и добавляем ее в список с ссылками product_urls
+            product_urls.append(urllib.parse.urljoin(base='https://fix-price.com', url=product_url))
+            # далее передаем объект Request по каждому адресу(по каждой ссылке на отдельный товар
+            # в метод обратного вызова parse_product
+            for url in product_urls:
+                yield scrapy.Request(url=url, callback=self.parse_product)
+
+    def parse_product(self, response):
+        """
+        Метод обратного вызова.
+        Извлекает информацию о каждом товаре c его страницы.
+        """
+        #data_products = []
+
+        products_data = {
+            'title': response.css('h1.title::text').get()
+        }
+        yield ProductItem(products_data)
 
 
 
-        #     item = ProductItem()
-        #     item['title'] = response.css('div.product__wrapper a.title::text').get()
-        # yield item
+
+
+
+ # next_page = response.css('a.next::attr(href)').get()
+        # yield response.follow(next_page, callback=self.parse_product)
+        #     # если же кол-во ссылок достигло 70, то работаем с каждой ссылкой
+        #     # для получения информации о каждом продукте в следующем методе
+        #     # - parse_product
+        #     # for url in self.product_urls:
+        #     #     yield scrapy.Request(url=url, callback=self.parse_product)
