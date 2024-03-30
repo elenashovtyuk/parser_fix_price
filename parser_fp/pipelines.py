9c4e54d0e8353bd0b24e7dea004a7e6d078fd0cc
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2024-03-30 20:43:56 +0300

    changes in pipelines

--- parser_fp/pipelines.py
+++ parser_fp/pipelines.py
@@ -1,11 +1,4 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# useful for handling different item types with a single interface
-# from itemadapter import ItemAdapter
+from itemadapter import ItemAdapter
 import json
 
 
@@ -18,7 +11,7 @@ class ParserFpPipeline:
             return item
 
     def open_spider(self, spider):
-        self.file = open('data.json', 'w')
+        self.file = open('data.json', 'w', encoding='utf-8')
 
     def close_spider(self, spider):
         self.file.close()
